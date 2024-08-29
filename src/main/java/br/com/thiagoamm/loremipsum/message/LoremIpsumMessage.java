adb999f6532b84317b0fcaf5e03e423c54f46eae
Thiago Monteiro <thicmp@gmail.com>
2024-08-29 15:07:39 -0300

    Created interaction messages related to the lorem ipsum file.

--- src/main/java/br/com/thiagoamm/loremipsum/message/LoremIpsumMessage.java
+++ src/main/java/br/com/thiagoamm/loremipsum/message/LoremIpsumMessage.java
@@ -9,6 +9,8 @@ public final class LoremIpsumMessage {
 
     private static final String AMOUNT_CANNOT_BE_LOWER_THAN = "Amount of %s cannot be lower than %d!";
     private static final String AMOUNT_CANNOT_BE_GREATER_THAN = "Amount of %s cannot be greater than %d!";
+    public static final String FILE_NAME_IS_NULL = "File name is null!";
+    public static final String FILE_NAME_IS_BLANK = "File name is blank!";
 
     private LoremIpsumMessage() {
         throw new UnsupportedOperationException();
@@ -21,4 +23,5 @@ public final class LoremIpsumMessage {
     public static String messageAmountCannotBeGreaterThanLimit(TextualEntity target, int limit) {
         return String.format(AMOUNT_CANNOT_BE_GREATER_THAN, target.getPlural().toLowerCase(), limit);
     }
+
 }
