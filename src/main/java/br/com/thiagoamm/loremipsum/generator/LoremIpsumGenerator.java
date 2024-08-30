399044124f34bbaff46fd85bca43a1b2d3d52611
Thiago Monteiro <thicmp@gmail.com>
2024-08-29 21:48:40 -0300

    Added methods for generating textual entities with fixed quantity or variable quantity.

--- src/main/java/br/com/thiagoamm/loremipsum/generator/LoremIpsumGenerator.java
+++ src/main/java/br/com/thiagoamm/loremipsum/generator/LoremIpsumGenerator.java
@@ -7,7 +7,10 @@ import java.util.List;
  * the operations that a lorem ipsum text generator must have.
  */
 public interface LoremIpsumGenerator {
-    List<String> generateParagraphs(int amount);
-    List<String> generateSentences(int amount);
     List<String> generateWords(int amount);
+    List<String> generateWordsBetween(int minAmount, int maxAmount);
+    List<String> generateSentences(int amount);
+    List<String> generateSentencesBetween(int minAmount, int maxAmount);
+    List<String> generateParagraphs(int amount);
+    List<String> generateParagraphsBetween(int minAmount, int maxAmount);
 }
