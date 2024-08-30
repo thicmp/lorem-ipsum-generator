c441ad684f4042aac6f4a80eccd44f025eff7016
Thiago Monteiro <thicmp@gmail.com>
2024-08-30 08:41:21 -0300

    Added contracts for implementing methods for generating shuffled words.

--- src/main/java/br/com/thiagoamm/loremipsum/generator/LoremIpsumGenerator.java
+++ src/main/java/br/com/thiagoamm/loremipsum/generator/LoremIpsumGenerator.java
@@ -9,8 +9,14 @@ import java.util.List;
 public interface LoremIpsumGenerator {
     List<String> generateWords(int amount);
     List<String> generateWordsBetween(int minAmount, int maxAmount);
+    List<String> generateShuffleWords(int amount);
+    List<String> generateShuffleWordsBetween(int minAmount, int maxAmount);
     List<String> generateSentences(int amount);
     List<String> generateSentencesBetween(int minAmount, int maxAmount);
+    List<String> generateShuffleSentences(int amount);
+    List<String> generateShuffleSentencesBetween(int minAmount, int maxAmount);
     List<String> generateParagraphs(int amount);
     List<String> generateParagraphsBetween(int minAmount, int maxAmount);
+    List<String> generateShuffleParagraphs(int amount);
+    List<String> generateShuffleParagraphsBetween(int minAmount, int maxAmount);
 }
